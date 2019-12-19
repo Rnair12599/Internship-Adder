ae0937de71f0cd62f6088a88b6137c843fb72eb9
Rahul Nair <rnair12599@gmail.com>
2019-12-19 00:40:09 -0800

    Used pandas for dataframes and included google sheets updating feature

--- InternshipAdder.py
+++ InternshipAdder.py
@@ -1,11 +1,8 @@
 import datetime
-
-class InterApp:
-    def __init__(self, company, type, date, status):
-        self.company = company
-        self.type = type
-        self.date = date
-        self.status = status
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
 
 def printMen():
     print("If you want to add a new input, enter '1'")
@@ -17,46 +14,70 @@ def getDate():
     x = datetime.datetime.now()
     return x.strftime("%x")
 
+def toGSheet(df):
+    scope = ["https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name("../APIcreds/InternshipAdder-DriveAPI-Cred.json", scope)
+    #getting credentials from json credential file for google drive and google sheets
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open("Summer2019_Internships").sheet1  # Open the spreadsheet
+
+    fp = open("InternshipList.csv", "r")
+    line = fp.readline()
+    cnt = 0
+    while line:
+        currArr = line.split(",")
+        cnt += 1
+        for i in range(len(currArr)):
+            sheet.update_cell(cnt,i+1, currArr[i])
+        line = fp.readline()
+
+    fp.close()
+
+    """data = sheet.get_all_records()  # Get a list of all records
+
+    row = sheet.row_values(3)  # Get a specific row
+    col = sheet.col_values(3)  # Get a specific column
+    cell = sheet.cell(1,2).value  # Get the value of a specific cell
+
+    insertRow = ["hello", 5, "red", "blue"]
+    sheet.add_rows(insertRow, 4)  # Insert the list as a row at index 4
+
+    sheet.update_cell(2,2, "CHANGED")  # Update one cell
+
+    numRows = sheet.row_count  # Get the number of rows in the sheet"""
+
 #--------Start of MAIN PROGRAM-------------
-f = open("InternshipList.txt", "r")
 
-line = f.readline()
+df = pd.read_csv("InternshipList.csv")
+print(df)
 
-
-AllApp = []
-
-while line:
-    tempL = line.split(", ")
-    sCom = str(tempL[0])
-    sType = str(tempL[1])
-    sDate = str(tempL[2])
-    sStatus = str(tempL[len(tempL) - 1])
-    apps = InterApp(sCom,sType,sDate,sStatus)
-    AllApp.append(apps)
-    line = f.readline()
-f.close()
 
 val = -1
+Additions = []
+
 while(val != 0):
     val = printMen()
     if(val == 1):
-        sCom = str(input("Enter Company Name: "))
-        sType = str(input("Enter what type of internship: "))
+        temp = []
+        temp.append(str(input("Enter Company Name: ")).capitalize())
+        temp.append(str(input("Enter what type of internship: ")))
         sDate = str(input("Enter date applied or 't' for today: "))
-        sStatus = str(input("Enter the status of application"))
-        sStatus += "\n"
         if(sDate == 't'):
             sDate = str(getDate())
-        Rec = InterApp(sCom,sType,sDate,sStatus)
-        AllApp.append(Rec)
+        temp.append(sDate)
+        temp.append(str(input("Enter the status of application: ")))
+        temp.append(str(input("Enter stage currently at for application: ")))
+        Additions.append(temp)
 
+df2 = pd.DataFrame(Additions, columns = ['Company','Position','Date','Status','Stage'])
+df = df.append(df2)
+df.sort_values("Company", inplace = True)
+df.to_csv("InternshipList.csv", index = False)
 
-AllApp.sort(key = lambda x: x.company)
-
-f = open("InternshipList.txt", "w")
-
-for i in AllApp:
-    f.write(i.company + ", ")
-    f.write(i.type + ", ")
-    f.write(i.date + ", ")
-    f.write(i.status)
+toGSheet(df)
